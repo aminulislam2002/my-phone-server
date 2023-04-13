a6b68ae5497f8617b533e543239b8099473c0468
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-04-13 09:23:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+var cors = require("cors");
+const phones = require("./phones.json");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("abcd");
+});
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("I need single data by id", id);
+  const phone = phones.find((phone) => phone.id === id) || {};
+  res.send(phone);
+});
+
+app.listen(port, () => {
+  console.log(`My server is running on port: ${port}`);
+});
